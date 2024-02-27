5fefcb5b65e51197ec5252bf04535ef28c7d7daa
deardeer123 <adad4852@gmail.com>
2024-02-27 17:33:38 +0900

    recommitSorry

--- src/main/java/com/green/Library/library/buy/service/BuyServiceImpl.java
+++ src/main/java/com/green/Library/library/buy/service/BuyServiceImpl.java
@@ -1,8 +1,24 @@
 package com.green.Library.library.buy.service;
 
+import com.green.Library.libraryBook.vo.LibraryBookVO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service("buyService")
 public class BuyServiceImpl implements BuyService{
+    @Autowired
+    SqlSessionTemplate sqlSession;
 
+    //책 등록하기
+    //책 등록하기전에 책 먼저부터 등록한다음
+    //그다음에 책 상세정보 등록하기
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void regBook(LibraryBookVO libraryBookVO) {
+        sqlSession.insert("buyMapper.regBook", libraryBookVO);
+        sqlSession.insert("buyMapper.regBookInfo", libraryBookVO);
+
+    }
 }
--- src/main/java/com/green/Library/library/regAndView/service/RegAndViewServiceImpl.java
+++ src/main/java/com/green/Library/library/regAndView/service/RegAndViewServiceImpl.java
@@ -1,2 +1,21 @@
-package com.green.Library.library.regAndView.service;public class RegAndViewServiceImpl {
+package com.green.Library.library.regAndView.service;
+
+import com.green.Library.libraryBook.vo.LibraryBookVO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("regAndViewService")
+public class RegAndViewServiceImpl implements RegAndViewService{
+    @Autowired
+    SqlSessionTemplate sqlSession;
+
+
+    //왜 책 조회가 안될까?
+    @Override
+    public List<LibraryBookVO> selectBookList() {
+        return sqlSession.selectList("regAndViewMapper.selectBookList");
+    }
 }
