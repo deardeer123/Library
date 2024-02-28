c7f83f980d5e635ef55ce647358082b38e2e455c
namjiijii <namji6565@gmail.com>
2024-02-28 15:34:48 +0900

    수정

--- src/main/java/com/green/Library/web/culturalAndEducation/service/CulturalAndEducationService.java
+++ src/main/java/com/green/Library/web/culturalAndEducation/service/CulturalAndEducationService.java
@@ -1,4 +1,15 @@
 package com.green.Library.web.culturalAndEducation.service;
 
+import com.green.Library.web.culturalAndEducation.vo.CulturalAndEducationVO;
+import com.green.Library.web.participationForum.vo.ParticipationForumVO;
+
+import java.util.List;
+
 public interface CulturalAndEducationService {
+
+    //문화 게시판 리스트
+    List<CulturalAndEducationVO> selectCulBoardList();
+
+    //문화 게시판 등록
+    void insertCulBoard(CulturalAndEducationVO culturalAndEducationVO);
 }
---
+++ src/main/java/com/green/Library/web/culturalAndEducation/service/CulturalAndEducationServiceImpl.java
@@ -0,0 +1,25 @@
+package com.green.Library.web.culturalAndEducation.service;
+
+import com.green.Library.web.culturalAndEducation.vo.CulturalAndEducationVO;
+import com.green.Library.web.participationForum.vo.ParticipationForumVO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("culturalAndEducationService")
+public class CulturalAndEducationServiceImpl implements CulturalAndEducationService{
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+    @Override
+    public List<CulturalAndEducationVO> selectCulBoardList() {
+        return sqlSession.selectList("culturalAndEducationMapper.selectCulBoardList");
+    }
+
+    @Override
+    public void insertCulBoard(CulturalAndEducationVO culturalAndEducationVO) {
+        sqlSession.insert("culturalAndEducationMapper.insertCulBoard",culturalAndEducationVO);
+    }
+}
