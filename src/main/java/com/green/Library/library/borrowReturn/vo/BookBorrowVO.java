b6123d51be49da9c6e609b79e4931958cc9f5c00
Bom-E <j32575086@gmail.com>
2024-03-06 16:42:36 +0900

    03.06

--- src/main/java/com/green/Library/library/borrowReturn/vo/BookBorrowVO.java
+++ src/main/java/com/green/Library/library/borrowReturn/vo/BookBorrowVO.java
@@ -1,5 +1,6 @@
 package com.green.Library.library.borrowReturn.vo;
 
+import com.green.Library.libraryBook.vo.LibraryBookInfoVO;
 import com.green.Library.libraryBook.vo.LibraryBookVO;
 import com.green.Library.libraryMember.vo.LibraryMemberVO;
 import com.green.Library.web.member.vo.MemberVO;
@@ -18,4 +19,5 @@ public class BookBorrowVO {
     private int userCode;
     private String bookCode;
     private LibraryBookVO libraryBookVO;
+    private LibraryBookInfoVO libraryBookInfoVO;
 }
