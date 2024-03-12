65aad7cd2fb525095284b2020bbf12584629db6e
deardeer123 <adad4852@gmail.com>
2024-03-12 16:15:33 +0900

    asd

--- src/main/java/com/green/Library/library/controller/LibraryController.java
+++ src/main/java/com/green/Library/library/controller/LibraryController.java
@@ -1,4 +1,13 @@
 package com.green.Library.library.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LibraryController {
-}
+
+    @GetMapping("/")
+    public String goHome(){
+        System.out.println("홈");
+        return "content/library/home";
+    }
