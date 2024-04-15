06ab5a714e460649740ba9901529f0f101c546da
Bom-E <j32575086@gmail.com>
2024-04-15 15:47:03 +0900

    24.04.15

--- src/main/java/com/green/Library/library/user/controller/UserController.java
+++ src/main/java/com/green/Library/library/user/controller/UserController.java
@@ -25,19 +25,8 @@ public class UserController {
 
     //-----------이용자------------------
     //이용자 관리
-    @GetMapping("/user")
-    public String goUser(Model model){
-        //이동하기전 메뉴리스트 가져가기
-        model.addAttribute("menuList", libraryMenuService.selectLibraryMenuList());
-
-
-        System.out.println(" 이동");
-        return "content/library/user/user";
-    }
-
-    // 이용자 관리 검색 기능
-    @RequestMapping("/letUserSearch")
-    public String letUserSearch(Model model, SearchUserVO searchUserVO){
+    @RequestMapping("/user")
+    public String goUser(Model model, SearchUserVO searchUserVO){
         //이동하기전 메뉴리스트 가져가기
         model.addAttribute("menuList", libraryMenuService.selectLibraryMenuList());
 
@@ -55,6 +44,8 @@ public class UserController {
 
         model.addAttribute("membersInfo", filterIsCardNum);
 
+
+        System.out.println(" 이동");
         return "content/library/user/user";
     }
 
@@ -76,19 +67,9 @@ public class UserController {
     }
 
     //이용자 승인
-    @GetMapping("/userApproval")
-    public String goUserApproval(Model model){
+    @RequestMapping("/userApproval")
+    public String goUserApproval(Model model, SearchUserVO searchUserVO){
         //이동하기전 메뉴리스트 가져가기
-        model.addAttribute("menuList", libraryMenuService.selectLibraryMenuList());
-
-        System.out.println("이용자 승인 이동");
-        return "content/library/user/userApproval";
-    }
-
-    // 이용자 승인 페이지 조회(cardNum 부여 하기 위해 우선 select)
-    @RequestMapping("/letUserApproval")
-    public String letUserApproval(Model model, SearchUserVO searchUserVO){
-
         model.addAttribute("menuList", libraryMenuService.selectLibraryMenuList());
 
         // 이용자 정보 전체 조회
@@ -108,6 +89,7 @@ public class UserController {
 
         System.out.println(filterNoCardNum);
 
+        System.out.println("이용자 승인 이동");
         return "content/library/user/userApproval";
     }
 
@@ -118,7 +100,7 @@ public class UserController {
         // cardNum 업데이트 쿼리
         userService.updateCardNum(memberVO);
 
-        return "redirect:/bookAdmin/letUserApproval";
+        return "redirect:/bookAdmin/userApproval";
     }
 
     //연체자 관리
