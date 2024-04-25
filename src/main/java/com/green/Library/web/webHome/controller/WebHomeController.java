20ede581f4798b9a381ef99da790919477d065ac
deardeer123 <adad4852@gmail.com>
2024-04-25 16:01:17 +0900

    asdadsgasgd

--- src/main/java/com/green/Library/web/webHome/controller/WebHomeController.java
+++ src/main/java/com/green/Library/web/webHome/controller/WebHomeController.java
@@ -13,10 +13,7 @@ import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -112,5 +109,11 @@ public class WebHomeController {
         //System.out.println(mapList);
 
         return mapList;
+    }
+
+    @GetMapping("/goNotice")
+    public String goNotice(@RequestParam(name = "boardNum")int boardNum){
+
+        return "redirect:/noticeDetail?boardNum="+boardNum;
     }
 }
