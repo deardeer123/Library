568fde1a999c4133cd711d744c4eef94f3cabbbe
Bom-E <j32575086@gmail.com>
2024-05-17 11:48:53 +0900

    240517

--- src/main/java/com/green/Library/web/findBook/controller/FindBookController.java
+++ src/main/java/com/green/Library/web/findBook/controller/FindBookController.java
@@ -99,15 +99,23 @@ public class FindBookController {
 
 
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@" + borrowReturnService.selectBookAvailable(bookCode));
+        System.out.println("##########################" + findBookService.selectDuplication(bookReservationVO));
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&" + borrowReturnService.selectChkReservation(bookCode));
 
 
-        if (!session.getAttribute("userCode").equals("") && !findBookService.selectDuplication(bookReservationVO) && borrowReturnService.selectBookAvailable(bookCode) || !borrowReturnService.selectChkReservation(bookCode).isEmpty()) {
+        if ((!session.getAttribute("userCode").equals("") && !findBookService.selectDuplication(bookReservationVO) && borrowReturnService.selectBookAvailable(bookCode)) && borrowReturnService.selectChkReservation(bookCode).isEmpty()) {
 
             findBookService.bookReservationFetch1(bookReservationVO);
 
             return "get";
 
-        } else if(!session.getAttribute("userCode").equals("") && !findBookService.selectDuplication(bookReservationVO) && !borrowReturnService.selectBookAvailable(bookCode) || !borrowReturnService.selectChkReservation(bookCode).isEmpty()){
+        } else if ((!session.getAttribute("userCode").equals("") && !findBookService.selectDuplication(bookReservationVO) && !borrowReturnService.selectBookAvailable(bookCode)) && borrowReturnService.selectChkReservation(bookCode).isEmpty()) {
+
+            findBookService.bookReservationFetch2(bookReservationVO);
+
+            return "first-wait";
+
+        } else if((!session.getAttribute("userCode").equals("") && !findBookService.selectDuplication(bookReservationVO) && !borrowReturnService.selectBookAvailable(bookCode)) && !borrowReturnService.selectChkReservation(bookCode).isEmpty()){
 
             findBookService.bookReservationFetch2(bookReservationVO);
 
